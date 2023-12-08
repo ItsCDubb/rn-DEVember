ade2c0821eaa125f04f69489fcc4d993ff77d2e1
Chris Warren <chris.warren.2019@gmail.com>
2023-12-08 16:19:07 -0500

    Updated the Day 1 Detail Screen & created the Day 2 Detail screen

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,5 +1,4 @@
 import { Text, View } from "react-native";
-import React from "react";
 import { Stack } from "expo-router";
 
 const DayDetailsScreen = () => {
--- src/app/(days)/day2/index.tsx
+++ src/app/(days)/day2/index.tsx
@@ -1,10 +1,20 @@
 import { Text, View } from "react-native";
-import React from "react";
+import { Stack } from "expo-router";
 
 const DayDetailsScreen = () => {
   return (
     <View>
-      <Text>Day Details Screen</Text>
+      <Stack.Screen options={{ title: "Day 2" }} />
+      <Text style={{ fontFamily: "AmaticBold", fontSize: 60 }}>
+        Animated Onboarding Flow
+      </Text>
+      <Text style={{ fontFamily: "Inter", fontSize: 30 }}>Onboarding</Text>
+      <View>
+        <Text>
+          Today animated onboard screens were created for the project that I've
+          been working on for #DEVember.
+        </Text>
+      </View>
     </View>
   );
 };
