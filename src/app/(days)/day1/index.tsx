0dae34564dbf758966a05855256fd2ac3dc4a726
Chris Warren <chris.warren.2019@gmail.com>
2023-12-08 11:43:45 -0500

    Stack Navigator

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const DayDetailsScreen = () => {
   return (
     <View>
+      <Stack.Screen options={{ title: "Day 1" }} />
       <Text>Day Details Screen</Text>
     </View>
   );
