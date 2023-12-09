7a3f26c12c0f252c16cdf196d0538096a828f96f
Chris Warren <chris.warren.2019@gmail.com>
2023-12-09 13:33:50 -0500

    Adjusted the text on the Day 2 page

--- src/app/(days)/day2/index.tsx
+++ src/app/(days)/day2/index.tsx
@@ -8,6 +8,13 @@ const DayDetailsScreen = () => {
       <Text style={{ fontFamily: "AmaticBold", fontSize: 60 }}>
         Animated Onboarding Flow
       </Text>
+      <View>
+        <Text style={{ padding: 7 }}>
+          Today added a few Animated Onboarding Screens using Expo Icons, React
+          Gestures, and Reanimated. Nothing super complex, but pretty
+          nonetheless.
+        </Text>
+      </View>
       <Link href="/day2/onboarding" asChild>
         <Button title="Onboarding Screens" />
       </Link>
