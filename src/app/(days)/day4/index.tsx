222d1d00f829447daca291596f3b1fbc4110157f
Chris Warren <chris.warren.2019@gmail.com>
2023-12-12 10:38:47 -0500

    Day 4 completed

---
+++ src/app/(days)/day4/index.tsx
@@ -0,0 +1,29 @@
+import { Button, SafeAreaView, Text, View } from "react-native";
+import { Link, Stack } from "expo-router";
+import MarkdownDisplay from "@/components/day3/MarkdownDisplay";
+import styles from "./styles";
+
+const description = `
+# Animated splash screen
+📚 Today's Agenda:
+- Overview of Splash Screens in Apps
+- Setting Up the Default Splash Screen
+- Designing a Custom Splash Screen
+- Integrating Lottie Animations
+- Configuring Lottie with React Native and Expo
+- Best Practices for Splash Screen Animations
+`;
+
+const DayDetailsScreen = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: "Day 4: Animated Splash Screen" }} />
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+      <Link href="/day4/animation" asChild>
+        <Button title="Animation" />
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default DayDetailsScreen;
---
+++ src/app/(days)/day4/splash.tsx
@@ -0,0 +1,31 @@
+import { View } from "react-native";
+import LottieView from "lottie-react-native";
+import { useRef } from "react";
+import { Stack } from "expo-router";
+
+const AnimationScreen = () => {
+  const animation = useRef<LottieView>(null);
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <Stack.Screen options={{ headerShown: false }} />
+      <LottieView
+        ref={animation}
+        style={{
+          width: "80%",
+          maxWidth: 400,
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require("@assets/lottie/netflixAnimation.json")}
+      />
+    </View>
+  );
+};
+
+export default AnimationScreen;
